15cd9e76b7fee2f8d943746b36b89781f0bcc443
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2022-07-18 03:12:36 -0700

    Initial File Reading
    Worked on extending the file reading functionality, and now I almost have it so you can read the contents of the file (in the console as of now)! Next I am going to figure out how you can get the result and forward it to the UI, as I'd like it to show up in the `TextEditor()`.
    
    A few more links helped out again today:
    https://www.youtube.com/watch?app=desktop&v=q8y_eRVfpMA
    https://stackoverflow.com/questions/64149342/how-to-add-file-picker-to-the-app-on-ios-14-and-lower
    https://stackoverflow.com/questions/31352593/how-to-print-details-of-a-catch-all-exception-in-swift

--- FilePicker.swift
+++ FilePicker.swift
@@ -3,10 +3,33 @@ import SwiftUI
 struct FilePicker: UIViewControllerRepresentable {
     @Binding var content: String
     
+    func makeCoordinator() -> FilePicker.Coordinator {
+        return FilePicker.Coordinator(parent: self)
+    }
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<FilePicker>) -> UIDocumentPickerViewController {
         let picker = UIDocumentPickerViewController(forOpeningContentTypes: [.text])
+        picker.delegate = context.coordinator
         return picker
     }
     
     func updateUIViewController(_ uiViewController: UIDocumentPickerViewController, context: UIViewControllerRepresentableContext<FilePicker>) {}
+    
+    class Coordinator: NSObject, UIDocumentPickerDelegate {
+        let picker: FilePicker
+        
+        init(parent: FilePicker) {
+            picker = parent
+        }
+        
+        func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+            let url = urls[0]
+            do {
+                let content = try Data(contentsOf: url)
+                print(content)
+            } catch {
+                print("Couldn't load file: \(error)")
+            }
+        }
+    }
 }
